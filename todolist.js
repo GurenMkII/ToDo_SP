50fa5446afe44a21ce2e8c0823eb175ea9a4a5c0
GurenMkII <62103374+GurenMkII@users.noreply.github.com>
2020-04-07 16:17:56 -0400

    cenverted code to jQuery, fixed whole list deletion

--- todolist.js
+++ todolist.js
@@ -1,33 +1,22 @@
-var todoInput = document.querySelector('input[type="text"]');
-var todoBtn = document.querySelector('#todobutton');
+$('#todoinput').keyup(function(){
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        $("#todobutton").click();
+      }
+    });
 
-
-// Press Enter instead of button click
-todoInput.addEventListener("keyup", function(event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      // Trigger the button element with a click
-      document.getElementById("todobutton").click();
-    }
-  });
-
-todoBtn.addEventListener('click', function(){
-    let li = document.createElement('li');
-    li.innerHTML = todoInput.value;
-    list.appendChild(li);
-    todoInput.value = '';
-
+$('button#todobutton').click(function(){
+  $('#list').append('<li>' + $('#todoinput').val() + '</li>');
+  $('#todoinput').val("");    
 })
-list.addEventListener('click', function(event){
-    event.target.style.color = 'red';
-    event.target.style.textDecoration = 'line-through';
+
+$('#list').click(function(event){
+  var target = $(event.target);
+  if (target.is('li')){
+  target.css({color:'red'});
+  target.wrap('<strike>');
     setTimeout(function(){
-        event.target.remove();
-    }, 1000)
-    
+        target.remove();
+    }, 1000); 
+  } 
 })
-
-
-
