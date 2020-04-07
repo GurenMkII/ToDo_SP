54c395391e74fe06b68abb908c4eee328f992cf2
GurenMkII <62103374+GurenMkII@users.noreply.github.com>
2020-04-07 16:46:17 -0400

    added comments and deleted a few unnecessary lines

--- todolist.js
+++ todolist.js
@@ -1,22 +1,21 @@
-$('#todoinput').keyup(function(){
-      if (event.keyCode === 13) {
-        event.preventDefault();
-        $("#todobutton").click();
+$('#todoinput').keyup(function(){ //executes function when key is released.
+      if (event.keyCode === 13) { //keyCode 13 is "Enter".
+        $("#todobutton").click(); 
       }
     });
 
-$('button#todobutton').click(function(){
-  $('#list').append('<li>' + $('#todoinput').val() + '</li>');
-  $('#todoinput').val("");    
+$('#todobutton').click(function(){ //function is executed when #todobutton is clicked.
+  $('#list').append('<li>' + $('#todoinput').val() + '</li>'); //creates and populate a new <li> element.
+  $('#todoinput').val("");    //clears the form input when #todobutton is clicked.
 })
 
 $('#list').click(function(event){
   var target = $(event.target);
-  if (target.is('li')){
-  target.css({color:'red'});
-  target.wrap('<strike>');
-    setTimeout(function(){
-        target.remove();
+  if (target.is('li')){ //.is is checking if target = 'li'.
+  target.css({color:'red'}); //applies css styles.
+  target.wrap('<strike>'); //wrap element in <strike>.
+    setTimeout(function(){ //delayed activation function.
+        target.remove(); //delete the target...
     }, 1000); 
   } 
 })
